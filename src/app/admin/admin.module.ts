fa4492aecf43e5c32550ba6f83ce064e6405e9e3
The Buildbot <buildbot@angular2buch.de>
2022-10-25 08:42:58 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/1457c505599396bf719ef625dd55aeb28dbbb895

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LocalIsoDateValueAccessorModule } from 'angular-date-value-accessor';
+import { LocalIsoDateValueAccessor } from 'angular-date-value-accessor';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { BookFormComponent } from './book-form/book-form.component';
@@ -20,7 +20,7 @@ import { FormErrorsComponent } from './form-errors/form-errors.component';
     CommonModule,
     AdminRoutingModule,
     ReactiveFormsModule,
-    LocalIsoDateValueAccessorModule
+    LocalIsoDateValueAccessor
   ],
 })
 export class AdminModule { }
