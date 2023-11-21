4470e6593bdfeaed4779225ca11bea6f16906349
The Buildbot <buildbot@angular2buch.de>
2023-11-21 14:40:56 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/23c2a481d87d8382e559eaa4c4a9f51edf2d2e39

---
+++ server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{b as o,c as r,d as n}from"./chunk-EEC7RGCE.mjs";import{fb as e}from"./chunk-USROMONB.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
